3757c07aae9b6a22056e9f86b2a1e568a55ad78b
Metintasarim4 <Lutfumetin@hotmail.com.tr>
2026-02-16 19:20:51 +0300

    Paketsiz sitemap oluşturucu eklendi ve harita güncellendi

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,41 +1,48 @@
 import fs from 'fs';
-import { create } from 'xmlbuilder2';
 
-// Senin için hazırladığım tüm bölgelerin listesi (Örnektir, listeyi buraya ekliyoruz)
+// Senin için hazırladığım bölgelerin listesi
 const regions = [
   { il: 'istanbul', ilce: 'kadikoy', mahalleler: ['moda', 'suadiye', 'erenkoy'] },
   { il: 'istanbul', ilce: 'besiktas', mahalleler: ['bebek', 'ortakoy', 'levent'] },
   { il: 'ankara', ilce: 'cankaya', mahalleler: ['ayranci', 'bahcelievler'] },
-  // Ben buraya diğer 81 ili ve ilçeleri otomatik ekleyecek yapıyı kuruyorum
+  // Diğer bölgeleri de buraya ekleyebilirsin
 ];
 
 const hostname = 'https://evdeneve.metintasarim.io';
 
-const root = create({ version: '1.0', encoding: 'UTF-8' })
-  .ele('urlset', { xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9' });
+// XML Başlangıcı
+let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
-// 1. Ana Sayfa ve Sabit Sayfalar
+// 1. Sabit Sayfalar
 const staticPages = ['', '/hizmet-bolgelerimiz', '/blog', '/site-haritasi'];
 staticPages.forEach(page => {
-  root.ele('url').ele('loc').txt(`${hostname}${page}`).up().ele('priority').txt('1.0');
+  xml += `
+  <url>
+    <loc>${hostname}${page}</loc>
+    <priority>1.0</priority>
+  </url>`;
 });
 
-// 2. Dinamik İl, İlçe ve Mahalle Sayfaları
+// 2. Dinamik Mahalle Sayfaları
 regions.forEach((item) => {
-  // Mahalle Sayfası Linki
   item.mahalleler.forEach((mahalle) => {
-    root.ele('url')
-      .ele('loc').txt(`${hostname}/${item.il}/${item.ilce}/${mahalle}`).up()
-      .ele('changefreq').txt('monthly').up()
-      .ele('priority').txt('0.6');
-      
-    // Ev Taşıma Özel Sayfası Linki
-    root.ele('url')
-      .ele('loc').txt(`${hostname}/ev-tasima/${item.il}/${item.ilce}/${mahalle}`).up()
-      .ele('priority').txt('0.5');
+    xml += `
+  <url>
+    <loc>${hostname}/${item.il}/${item.ilce}/${mahalle}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>${hostname}/ev-tasima/${item.il}/${item.ilce}/${mahalle}</loc>
+    <priority>0.5</priority>
+  </url>`;
   });
 });
 
-const xml = root.end({ prettyPrint: true });
+xml += `
+</urlset>`;
+
+// Dosyayı public klasörüne yaz
 fs.writeFileSync('./public/sitemap.xml', xml);
-console.log('Dev sitemap.xml başarıyla oluşturuldu!');
+console.log('Paketsiz dev sitemap.xml başarıyla oluşturuldu!');
